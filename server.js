116fc42593fda1cab2ed358ae8fd362ac08691b7
rakshajay <rakshashettyhs@gmail.com>
2024-11-12 16:54:41 -0800

    updated CORS_Origin

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ import webRouter from "./routes/webdev.js";
 const app = express();
 
 const { PORT, CORS_ORIGIN } = process.env;
+const allowedOrigins = CORS_ORIGIN.split(','); // Split CORS_ORIGIN by commas to get an array of origins
+
+// CORS configuration to allow multiple origins
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests) or from allowed origins
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 app.use(express.json()); // Parse incoming requests with JSON payloads
 app.use(express.static("public")); // Serve static files from the 'public' folder
-app.use(cors({ origin: CORS_ORIGIN })); // Allow cross-origin requests
 
 app.use(archiRouter); // Use the router as middleware
 app.use(webRouter);
